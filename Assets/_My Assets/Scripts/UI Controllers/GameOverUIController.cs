18579a8686963f7652f56d1c8dd4a75d3a6ea715
Anuj Sahu <anuj.s@theIntellify.com>
2025-06-03 12:47:56 +0530

    implement Game Over UI

--- Assets/_My Assets/Scripts/UI Controllers/GameOverUIController.cs
+++ Assets/_My Assets/Scripts/UI Controllers/GameOverUIController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,6 +10,11 @@ namespace FruitCutter
     {
         #region Variables
         [SerializeField] private Button restartBtn;
+        [SerializeField] private TextMeshProUGUI gameOverTxt;
+
+        private const float animationDuration = 0.4f;
+        [SerializeField] private float startFontSize;
+        [SerializeField] private float endFontSize;
         #endregion Variables
 
         #region Unity Methods
@@ -21,7 +27,14 @@ namespace FruitCutter
         {
 
         }
-
+        private void OnEnable()
+        {
+            StartGameOverAnimation();
+        }
+        private void OnDisable()
+        {
+            ResetGameOverTxt();
+        }
 
         #endregion Unity Methods
 
@@ -30,6 +43,35 @@ namespace FruitCutter
         {
             ActionManager.ToggleMainMenuUI?.Invoke(true);
             ActionManager.ToggleGameOverUI?.Invoke(false);
+        }
+        private void ResetGameOverTxt()
+        {
+            gameOverTxt.gameObject.SetActive(false);    
+            gameOverTxt.fontSize = startFontSize;
+        }
+        private void StartGameOverAnimation()
+        {
+            restartBtn.interactable = false;
+            StartCoroutine(AnimateText("Game Over"));
+        }
+        private IEnumerator AnimateText(string message)
+        {
+            gameOverTxt.text = message;
+            gameOverTxt.fontSize = startFontSize;
+            gameOverTxt.gameObject.SetActive(true);
+
+            float elapsedTime = 0f;
+            while (elapsedTime < animationDuration)
+            {
+                float t = elapsedTime / animationDuration;
+                float newSize = Mathf.Lerp(startFontSize, endFontSize, t);
+                gameOverTxt.fontSize = newSize;
+
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+            gameOverTxt.fontSize = endFontSize;
+            restartBtn.interactable = true;
         }
         #endregion Custom Methods
 
